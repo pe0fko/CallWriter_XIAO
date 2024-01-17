cd5fb1ddf12005e4aa5c8b7433a77083c52aab17
PE0FKO <pe0fko@gmail.com>
2024-01-17 16:41:14 +0100

    OK

--- CallWriter_XIAO_V2/CallWriter_XIAO_V2.ino
+++ CallWriter_XIAO_V2/CallWriter_XIAO_V2.ino
@@ -19,30 +19,30 @@
 #include "Font.h"
 #include "Filter.h"
 
-//#define   DEBUG
+#define   DEBUG
 
 const     float     SecondsOneChar            = 0.8;          // The number of seconds for TX of one charracter
-const     float     ToneBand                  =  250;         // 250 Bandbreete Hz
+const     float     ToneBand                  =  400;         // 250 Bandbreete Hz
 const     float     ToneLPFilter              = 1500;         // Lowpass filter cutoff frequency (100Hz below)
 const     uint32_t  ToneLines                 = 16;           // Number of tone carriers TX at the same time
-const     uint32_t  SampleRate                = 22000;        // 22KHz (max) sample rate
-const     float     ToneStep                  = ToneBand / ToneLines;
-const     float     ToneStart                 = ToneLPFilter - ToneBand;          // Tone start Hz
+const     uint32_t  SampleRate                = 16000;        // 22KHz (max) sample rate
+const     float     ToneStep                  = ToneBand / ToneLines;     // Tone steps in Hz
+const     float     ToneStart                 = ToneLPFilter - ToneBand;  // Tone start in Hz
 const     uint32_t  NextLineCount             = SecondsOneChar * SampleRate / FONT_LENGTH;
 const     uint32_t  SineTableLength           = 1 << 9;       // Length of sine table
 const     uint32_t  OscFraction               = 1 << 16;      // Oscilator 16bit fraction
 const     uint32_t  FilterTableLength         = 32;           // Length of filter table
-
-static    int64_t   Signal                    = 0;
-static    int32_t   SineTable[SineTableLength];
-static    uint32_t  OSCreg[ToneLines]         = { 0 };
-static    uint32_t  OSCincr[ToneLines]        = { 0 };
+volatile  uint32_t  SampleDAC                 = 0;            // New sample for the DAC output
+static    int32_t   SineTable[SineTableLength];               // Sinus table to use in DDS
+static    uint32_t  DDSToneAcc[ToneLines]     = { 0 };      
+static    uint32_t  DDSTonePhase[ToneLines]   = { 0 };
 static    uint8_t   const *pFontTable         = &FontTable[0];
 static    uint32_t  CharLine                  = 0;          // 16 bits of char line
 static    uint32_t  CharNextCount             = 0;          // SR count for next char line load.
 static    bool      GetNewSample              = true;
 static    bool      SampleOverflow            = false;
-static    Filter    FilterLP                  ;
+static    int16_t   FilterHistory[FILTER_TAP_NUM] = { 0 };
+static    uint32_t  FilterLastIndex           = 0;
 
 #ifdef DEBUG
 #define   printf    Serial.printf
@@ -69,26 +69,25 @@ void setup()
 
   printf("\n-- CallWriter V2.00 ------ PE0FKO --------\n");
 
-  Filter_init(&FilterLP);
-
   printf("Generate sine table, length=%d\n", SineTableLength);
   for(uint32_t i = 0; i < SineTableLength; i++)
   {
     float s = sin( (2.0 * M_PI * i) / SineTableLength );
-    SineTable[i] = (int32_t)(s * 512.0);         // [.10] -512.0 ... 512.0
+//    SineTable[i] = (int32_t)(s * 512.0);         // [.10] -512.0 ... 512.0
+    SineTable[i] = (int32_t)(s * 2000.0);         // [.12] -2000.0 ... 2000.0
   }
 
   printf("Generate tone tables, length=%d\n", ToneLines);
   for(uint32_t i = 0; i < ToneLines; i++)
   {
     // Start with shifted phase signals, more flat power pattern.
-    OSCreg[i]  = random(0, OscFraction * SineTableLength);  // [.25]
+    DDSToneAcc[i]  = random(0, OscFraction * SineTableLength);  // [.25]
 
     // Miror the text, or not
     uint32_t I = ToneLines - 1 - i;
 //    uint32_t I = i;
 
-    OSCincr[i] = (uint32_t)(        // [7.25] [7.16+9]
+    DDSTonePhase[i] = (uint32_t)(        // [7.25] [7.16+9]
        (uint64_t)                   // Need >37 bits
         (ToneStart + I * ToneStep)  // 12 bits (< 4096)
        * OscFraction                // 16 bits
@@ -113,11 +112,13 @@ void setup()
     float tone = ToneStart + i * ToneStep;
     printf( "Tone  %2d = %3.2fHz, +%2.4f (0x%08x) Incr, %.4f samp/tone.\n"
             , i, tone
-            , (float)OSCincr[i] / OscFraction 
-            , OSCincr[i]
+            , (float)DDSTonePhase[i] / OscFraction 
+            , DDSTonePhase[i]
             , (float)SampleRate / tone
           );
   }
+
+  printf("\n-- Starting the loop --------\n");
 
   TimerTc3.initialize(1000000UL / SampleRate);
   TimerTc3.attachInterrupt(timerIsr);
@@ -131,31 +132,29 @@ void loop()
 {
   if (GetNewSample)
   {
-    Signal = 0;
+    int Signal = 0;
+
     for(uint32_t i = 0; i < ToneLines; i++)
     {
-      OSCreg[i] += OSCincr[i];          // Calc the next oscilator value
+      DDSToneAcc[i] += DDSTonePhase[i];          // Calc the next oscilator value
       if (CharLine & (1u << i))         // Check if dot is needed.
-        Signal += SineTable[ (OSCreg[i] / OscFraction) % SineTableLength ]; // [4.10]
+        Signal += SineTable[ (DDSToneAcc[i] / OscFraction) % SineTableLength ]; // [.14]
     }
 
-    Signal /= ToneLines;                // [4.10] => [.10]
-
-    Filter_put(&FilterLP, Signal);      // Add low-pass 1.8KHz filter
-    Signal = Filter_get(&FilterLP);
+    Signal /= 1 << 8;                   // [4.14] => [.10]
+    Signal = FilterLP(Signal);          // Do the filter convolution
 
     if (Signal < -512) Signal = -512;   // Only 10 bits DAC in SAMD21
     if (Signal >  511) Signal =  511;   // Range -512 ... 511
-    Signal += 512;                      // Uplift negative value
-    Signal &= 0x3FF;                    // Hard set 10 bits
+
+    SampleDAC = (Signal+512) & 0x3FF;   // Uplist signal and (hard) set to 10 bits
+    GetNewSample = false;
 
     if (CharNextCount++ == NextLineCount) 
     {
       CharNextCount = 0;
       fontGetNextLine();
     }
-
-    GetNewSample = false;
   }
 
   static uint32_t millisStart = 0;
@@ -174,7 +173,7 @@ void loop()
 
 void timerIsr()
 {
-  analogWrite(A0, Signal);              // Output the analog signal
+  analogWrite(A0, SampleDAC);           // Output the analog signal
 
   if (GetNewSample && !SampleOverflow)  // Check overrun
   {
@@ -210,3 +209,55 @@ fontGetNextLine()
 //  CharLine ^= 0xFFFF;       // Inverse
 //  CharLine |= 0x8000;     // Underline the text
 }
+
+
+#if FILTER_TAP_NUM & 0x01
+
+void Filter_put(Filter* f, int input) {
+  f->history[f->last_index++] = input;
+  if(f->last_index == FILTER_TAP_NUM)
+    f->last_index = 0;
+}
+
+int Filter_get(Filter* f) {
+  long long acc = 0;
+  int index = f->last_index, i;
+  for(i = 0; i < FILTER_TAP_NUM; ++i) {
+    index = index != 0 ? index-1 : FILTER_TAP_NUM-1;
+    acc += (long long)f->history[index] * filter_taps[i];
+  };
+  return acc >> 16;
+}
+
+#else
+
+int FilterLP(int input)                         // Input 10bits signed
+{
+//  int32_t FilterACC = 0;                      // acc => 5b (0..32) + 10b sample + 16b taps
+  int64_t FilterACC = 0;                      // acc => 6b (0..64) + 10b sample + 16b taps
+
+  FilterHistory[(FilterLastIndex++) & (FILTER_TAP_NUM-1)] = input;    // [.10]
+
+  int index = FilterLastIndex;
+  for(int i = 0; i < FILTER_TAP_NUM; ++i) 
+  {
+//    FilterACC += (int32_t)FilterHistory[(index--) & 31] * FilterTaps[i];
+    FilterACC += (int32_t)FilterHistory[(index--) & (FILTER_TAP_NUM-1)] * FilterTaps[i];
+  };
+
+  return FilterACC >> FILTER_PRECISION;
+}
+
+/*
+
+    // Filter the signal on LP
+    int32_t FilterACC = 0;                      // acc => 5b (0..32) + 10b sample + 16b taps
+    int index = FilterLastIndex;
+    for(int i = 0; i < FILTER_TAP_NUM; ++i) 
+    {
+      int32_t his = FilterHistory[(index--) % FILTER_TAP_NUM];
+      FilterACC += his * FilterTaps[i];         // [10.0] * [0.14]
+    };
+    Signal = FilterACC >> FILTER_PRECISION;
+*/
+#endif
--- CallWriter_XIAO_V2/Filter.h
+++ CallWriter_XIAO_V2/Filter.h
@@ -2,36 +2,154 @@
 #define FILTER_H_
 
 /*
-
 FIR filter designed with
- http://t-filter.appspot.com
+http://t-filter.appspot.com
+*/
 
-sampling frequency: 22000 Hz
+#define FILTER_PRECISION    16
 
+//int32_t FilterTaps[FILTER_TAP_NUM] = {
+int32_t FilterTaps[] = {
+
+#if 0
+
+/*
+sampling frequency: 16000 Hz
 fixed point precision: 16 bits
 
-* 0 Hz - 1800 Hz
+* 0 Hz - 1600 Hz
   gain = 1
-  desired ripple = 3 dB
+  desired ripple = 2 dB
   actual ripple = n/a
 
-* 2300 Hz - 11000 Hz
+* 2000 Hz - 8000 Hz
   gain = 0
-  desired attenuation = -20 dB
+  desired attenuation = -30 dB
+  actual attenuation = n/a
+
+*/
+
+#define FILTER_TAP_NUM      64
+
+  6,
+  315,
+  227,
+  203,
+  97,
+  -55,
+  -193,
+  -249,
+  -182,
+  -9,
+  202,
+  345,
+  337,
+  151,
+  -148,
+  -428,
+  -541,
+  -394,
+  -11,
+  461,
+  796,
+  790,
+  360,
+  -382,
+  -1142,
+  -1528,
+  -1200,
+  -15,
+  1885,
+  4085,
+  6018,
+  7148,
+  7148,
+  6018,
+  4085,
+  1885,
+  -15,
+  -1200,
+  -1528,
+  -1142,
+  -382,
+  360,
+  790,
+  796,
+  461,
+  -11,
+  -394,
+  -541,
+  -428,
+  -148,
+  151,
+  337,
+  345,
+  202,
+  -9,
+  -182,
+  -249,
+  -193,
+  -55,
+  97,
+  203,
+  227,
+  315,
+  6
+
+#elif 1
+
+/*
+sampling frequency: 16000 Hz
+fixed point precision: 16 bits
+
+* 0 Hz - 1600 Hz
+  gain = 1
+  desired ripple = 2 dB
+  actual ripple = n/a
+
+* 2000 Hz - 8000 Hz
+  gain = 0
+  desired attenuation = -30 dB
   actual attenuation = n/a
 
 */
 
 #define FILTER_TAP_NUM 32
-#define FILTER_PRECISION 16
 
-typedef struct {
-  int32_t history[FILTER_TAP_NUM];
-  unsigned int last_index;
-} Filter;
+  -914,
+  246,
+  605,
+  1012,
+  1248,
+  1124,
+  582,
+  -249,
+  -1067,
+  -1484,
+  -1172,
+  5,
+  1898,
+  4091,
+  6016,
+  7142,
+  7142,
+  6016,
+  4091,
+  1898,
+  5,
+  -1172,
+  -1484,
+  -1067,
+  -249,
+  582,
+  1124,
+  1248,
+  1012,
+  605,
+  246,
+  -914
 
-void Filter_init(Filter* f);
-void Filter_put(Filter* f, int32_t input);
-int32_t Filter_get(Filter* f);
+#endif
+};
 
 #endif
